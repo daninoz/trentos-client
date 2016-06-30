dffdb6ac6b054d40d7d09ac677607c62999e0522
Daniel Albornoz <daniel@daninoz.com>
2016-06-29 22:09:01 -0300

    FIx stats

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -11,7 +11,6 @@
 
     initCharts();
 
-
     function getStatistics () {
       return $http.get('/api/events/statistics').then(function (response) {
         vm.statistics = response.data;
@@ -34,7 +33,7 @@
 
         vm.pieChart = {
           labels: vm.sports.map(function (sport) {
-            return sport.name
+            return sport.name;
           }),
           data: (function () {
             return vm.sports.map(function (sport) {
@@ -48,15 +47,15 @@
 
         vm.barChart = {
           labels: vm.sports.map(function (sport) {
-            return sport.name
+            return sport.name;
           }),
           series: ['Me gusta', 'Comentarios'],
           data: (function () {
             var data = [[],[]];
             vm.sports.forEach(function (sport) {
               var sportStats = getStatisticBySportId(sport.id);
-              data[0].push(parseInt(sportStats['likes'], 10));
-              data[1].push(parseInt(sportStats['comments'], 10));
+              data[0].push(parseInt(sportStats.likes, 10));
+              data[1].push(parseInt(sportStats.comments, 10));
             });
 
             return data;
@@ -67,7 +66,7 @@
 
     function getStatisticBySportId (id) {
       return vm.statistics.filter(function (stat) {
-        return stat.sport_id === id;
+        return parseInt(stat.sport_id, 10) === id;
       })[0];
     }
 
